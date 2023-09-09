5c988a0db725e5a9b8ad4f6327a92ce7e8ad67b1
dipanshu914 <122882167+dipanshu914@users.noreply.github.com>
2023-09-10 00:13:56 +0530

    "plot a simple graph of july 2014"

--- high_lows.py
+++ high_lows.py
@@ -1,9 +1,41 @@
 import csv
 
+import matplotlib.pyplot as plt
+
+
+# Get high temperatures from file.
 filename = "sitka_weather_07-2014.csv"
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+    # print(header_row)
+
+    """for index, column_header in enumerate(header_row):
+        print(index, column_header)"""
+
+    highs = []
+    for row in reader:
+        highs.append(row[1])
+
+    # print(highs)
+
+# plot data
+fig = plt.Figure(dpi=128, figsize=(100, 50))
+plt.plot(highs, c="blue")
+
+# format the plot.
+plt.title("Daily high Temperature of 2014 july:", fontsize=18)
+plt.xlabel("", fontsize=14)
+plt.ylabel("Temperature in (f)", fontsize=14)
+plt.tick_params(axis="both", which="major", labelsize="16")
+
+plt.show()
+
+
+
+
+
+
+
 
 
