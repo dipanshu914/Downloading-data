a9cb59dba122c55d95a6ccd3cc1407986528f82c
dipanshu914 <122882167+dipanshu914@users.noreply.github.com>
2023-09-10 16:28:19 +0530

    1st plot of graph of weather

--- high_lows.py
+++ high_lows.py
@@ -1,9 +1,11 @@
 import csv
 
-import matplotlib.pyplot as plt
+from datetime import datetime
+
+from matplotlib import pyplot as plt
 
 
-# Get high temperatures from file.
+# Get date and high temperatures from file.
 filename = "sitka_weather_07-2014.csv"
 with open(filename) as f:
     reader = csv.reader(f)
@@ -13,20 +15,25 @@ with open(filename) as f:
     """for index, column_header in enumerate(header_row):
         print(index, column_header)"""
 
-    highs = []
+    dates, highs = [], []
     for row in reader:
-        highs.append(row[1])
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+
+        high = int(row[1])
+        highs.append(high)
 
     # print(highs)
 
 # plot data
-fig = plt.Figure(dpi=128, figsize=(100, 50))
-plt.plot(highs, c="blue")
+fig = plt.figure(dpi=130, figsize=(10, 5))
+plt.plot(dates, highs, c="blue")
 
 # format the plot.
-plt.title("Daily high Temperature of 2014 july:", fontsize=18)
-plt.xlabel("", fontsize=14)
-plt.ylabel("Temperature in (f)", fontsize=14)
+plt.title("Daily high Temperature, of 2014 july:", fontsize=18, c="red")
+plt.xlabel("", fontsize=12)
+fig.autofmt_xdate()
+plt.ylabel("Temperature in (f)", fontsize=12)
 plt.tick_params(axis="both", which="major", labelsize="16")
 
 plt.show()
