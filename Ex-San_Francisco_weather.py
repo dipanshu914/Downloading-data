c9b8ff1c4cf59771c97f0b3a5a037b453bc97719
dipanshu914 <122882167+dipanshu914@users.noreply.github.com>
2023-09-18 22:22:38 +0530

    ex-franscisco file done

--- Ex-San_Francisco_weather.py
+++ Ex-San_Francisco_weather.py
@@ -1,5 +1,6 @@
 import csv
-
+from datetime import datetime
+from matplotlib import pyplot as plt
 
 # Creating and file
 filename = "sanfrancisco.csv"
@@ -13,8 +14,35 @@ with open(filename) as f:
     for index, column_header in enumerate(header_row):
         print(index, column_header)"""
 
+    dates, highs, lows = [], [], []
 
+    for row in reader:
+        if len(row) >= 4:  # Ensure the row has at least 4 columns
+            try:
+                current_date = datetime.strptime(row[1], "%Y-%m-%d")
+                high = int(row[2])
+                low = int(row[4])
+            except ValueError:
+                print(current_date, "missing data")
+            else:
+                dates.append(current_date)
+                highs.append(high)
+                lows.append(low)
 
+# Plot data
+fig = plt.figure(dpi=130, figsize=(10, 5))
+plt.plot(dates, highs, c="blue", alpha=0.5)
+plt.plot(dates, lows, c="red", alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor="red", alpha=0.4)
+
+# format a plot
+plt.title("San francisco_weather condition", fontsize=18, c="red")
+plt.xlabel("", fontsize=8)
+fig.autofmt_xdate()
+plt.ylabel("Temperature in {f}", fontsize=12)
+plt.tick_params(axis="both", which="major", labelsize="16")
+
+plt.show()
 
 
 
